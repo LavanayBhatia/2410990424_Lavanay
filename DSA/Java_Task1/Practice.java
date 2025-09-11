758cec7b8565d4782c573eaf1454682e363bf706
Lavanay Bhatia <vvenm333@gmail.com>
2025-09-11 21:55:32 +0530

    Create Practice.java

---
+++ DSA/Java_Task1/Practice.java
@@ -0,0 +1,35 @@
+//import java.util.Scanner;
+//
+//public class Practice {
+//    public static void main(String[] args) {
+//        for(int i=1;i<11;i++){
+//
+//
+//            if(i==10){
+//                System.out.print(i);
+//            }
+//            else{
+//                System.out.print(i+",");
+//            }
+//        }
+//        System.out.println();
+//        int i=1;
+//        while(true){
+//            System.out.println(i++);
+//            if(i==11){
+//                break;
+//            }
+//        }
+//        Scanner in=new Scanner(System.in);
+//        int x=in.nextInt();
+//        for(int i=1;i<11;i++) {
+//            if (x <= 0) {
+//                System.out.println("Invalid Number");
+//                break;
+//            } else {
+//                System.out.println(x + "*" + i + "=" + x * i);
+//            }
+//        }
+//    }
+//
+//}
