b0cd308c1e9397d50c8e28a0eed5539c37fa886a
Lavanay Bhatia <vvenm333@gmail.com>
2025-09-28 18:31:24 +0530

    Create DailyPractice2.java

---
+++ DSA/Java_Task2/DailyPractice2.java
@@ -0,0 +1,37 @@
+//import java.util.Scanner;
+//
+//public class Test{
+//  public static void main(String[] args) {
+//      Scanner in = new Scanner(System.in);
+//      int arr[]=new int[10];
+//for(int i=0;i<args.length;i++){
+//arr[i]=args[i].length();
+//}
+//int max=arr[0];
+//int pos=0;
+//for(int i=0;i< arr.length;i++){
+//    if(max<arr[i]){
+//        max=arr[i];
+//        pos=i;
+//    }
+//      }
+//      System.out.println(args[pos]);
+
+
+//      switch(args[1]){
+//          case "+"-> System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[2]));
+//          case "-"-> System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[2]));
+//          case "/"-> System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[2]));
+//          case "%"-> System.out.println(Integer.parseInt(args[0])%Integer.parseInt(args[2]));
+// }
+//      int a=in.nextInt(),b=in.nextInt(),pow=in.nextInt();
+//      int k=1;
+//      for (int i=0;i<pow;i++){
+//          k*=(a+b);
+//      }
+//      System.out.println(k);
+//      }
+//  }
+
+
+
